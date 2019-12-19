3a006c4443e42704b673130ef6417e5f838dd755
Nagaraju <dnr5dnr@gmail.com>
2019-12-19 06:37:55 +0530

    Updated with sout statement

--- src/col/Arralist_example.java
+++ src/col/Arralist_example.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 public class Arralist_example {
     public static void main(String[] args) {
+        System.out.println("This is to check the git changes");
         ArrayList<String> al = new ArrayList<>();
         al.add("java");
         al.add("c#");
